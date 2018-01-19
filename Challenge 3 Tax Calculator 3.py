958e8b17d8ab300d3c11a0852556c44db52178ce
Flash_W <phynew@hotmail.com>
2018-01-20 00:42:28 +0800

    Update Challenge 3 Tax Calculator 3.py

--- Challenge 3 Tax Calculator 3.py
+++ Challenge 3 Tax Calculator 3.py
@@ -1,21 +1,72 @@
 #! usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-class Config:
+import sys
+import csv # used to input csv file
 
-	def __init__(self, configfile):
-		self._config = {}
-		with open(configfile) as file:
-			for line in file：
-				fileLine = line.split('=')
-				print(fileLine)
+# processing command line parameters class
+
+class Args(object):
+
+	def __init__(self):
+		try:
+			self.args = sys.argv[1:]
+			index_config = args.index('-c')
+			self.configfile = args[index_config + 1]
+			index_user = args.index('-d')
+			self.userfile = args[index_user + 1]
+			index_salary = args.index('-o')
+			self.salaryfile = args[index_salary + 1]
+		except:
+			print('Parameter Error')
+
+arg = Args()
+
+class Config(object):
+
+	def __init__(self):
+		self.config = self._read_config()
+
+	def _read_config(self):
+		config = {}
+
+		try:
+			with open(arg.configfile) as file：
+				while True:
+					fileLine = file.readline()
+					if fileLine == None:
+						break
+					fileReplace = fileLine.replace(' ', '')
+					fileDictList = fileReplace.split('=')
+					config[fileDict[0]] = fileDict[1]
+		except:
+			print('Parameter Error')
+
+class UserData(object):
+
+	def __init__(self):
+		self.userdata = self._read_users_data()
+
+	def _read_users_data(self):
+		userdata = []
+
+		try:
+			with open(arg.userfile) as file:
+				while True:
+					fileLine = file.readline()
+				if fileLine == None:
+					break
 				i = 0
-				
-				for elem in fileLine:
-					elemStr = str(elem)
-					if i = 0:
-						dictKey = elem.strip( /n)
-						i += 1
-					else:
-						dictValue = elem
-				
-				self._config[dictKey] = dictValue
+				fileTupleList = fileLine.split(',')
+				userTuple = (fileTupleList[0], fileTupleList[1])
+				userdata[i] = userTuple
+				i += 1
+		except:
+			print('Parameter Error')
+
+class IncomeTaxCalculator(object):
+	
+	def calc_for_all_userdata(self):
+		
+		
+
